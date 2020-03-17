f5b99b74e0bac07bfcd804af65212455a29b617c
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2020-03-17 00:54:10 +0000

    Improve jenkins module

--- build/automation/lib/jenkins.mk
+++ build/automation/lib/jenkins.mk
@@ -1,9 +1,18 @@
-jenkins-upload-workspace:
+jenkins-upload-workspace: ### Upload the project workspace as a file archive to a storage
 	eval "$$(make aws-assume-role-export-variables)"
 	make aws-session-fail-if-invalid
-	aws s3 cp $(WORKSPACE) \
-		s3://$(JENKINS_WORKSPACE_BUCKET)/$(BUILD_BRANCH)_$(BUILD_NUMBER) \
-		--recursive \
-		--exclude ".git/*" \
-		--quiet
-	echo "Jenkins Workspace Bucket URL in Non Prod is: https://s3.console.aws.amazon.com/s3/buckets/$(JENKINS_WORKSPACE_BUCKET)/$(BUILD_BRANCH)_$(BUILD_NUMBER)/"
+	date=$$(date --date=$(BUILD_DATE) -u +"%Y%m%d%H%M%S")
+	id=$$(printf "%04d\n" $(BUILD_ID))
+	file=workspace-$(PROJECT_NAME_SHORT)-$${date}-$${id}-$(BUILD_HASH).tar.gz
+	tar --exclude-vcs --exclude='$(TMP_DIR_REL)' -zcvf $(TMP_DIR)/$$file .
+	make _jenkins-create-workspace-storage
+	make aws-s3-upload \
+		FILE=$(TMP_DIR_REL)/$$file \
+		URI=$(JENKINS_WORKSPACE_BUCKET)/$(BUILD_BRANCH)/$$file
+	rm -f $(TMP_DIR)/workspace-$(PROJECT_NAME_SHORT)-*
+	echo "Jenkins workspace URL is https://s3.console.aws.amazon.com/s3/object/$(JENKINS_WORKSPACE_BUCKET)/$(BUILD_BRANCH)/$$file"
+
+_jenkins-create-workspace-storage:
+	if [ false == $$(make aws-s3-exists NAME=$(JENKINS_WORKSPACE_BUCKET)) ]; then
+		make aws-s3-create NAME=$(JENKINS_WORKSPACE_BUCKET)
+	fi
--- build/automation/test/jenkins.test.mk
+++ build/automation/test/jenkins.test.mk
@@ -1,7 +1,25 @@
 test-jenkins: \
-	test-jenkins-upload-workspace
+	test-jenkins-setup \
+	test-jenkins-upload-workspace \
+	test-jenkins-teardown
+
+test-jenkins-setup:
+	make docker-config
+	make docker-compose-start YML=$(TEST_DIR)/docker-compose.localstack.yml
+
+test-jenkins-teardown:
+	make docker-compose-stop YML=$(TEST_DIR)/docker-compose.localstack.yml
+	rm -rf \
+		$(TMP_DIR)/localstack \
+		$(TMP_DIR)/workspace-*.tar.gz.download
 
 # ==============================================================================
 
 test-jenkins-upload-workspace:
-	mk_test_skip $(@) ||:
+	# act
+	make jenkins-upload-workspace
+	# assert
+	make aws-s3-download \
+		URI=$(JENKINS_WORKSPACE_BUCKET)/$(BUILD_BRANCH)/workspace-$(PROJECT_NAME_SHORT)-$$(date --date=$(BUILD_DATE) -u +"%Y%m%d%H%M%S")-$$(printf "%04d\n" $(BUILD_ID))-$(BUILD_HASH).tar.gz \
+		FILE=$(TMP_DIR_REL)/workspace-$(PROJECT_NAME_SHORT)-$$(date --date=$(BUILD_DATE) -u +"%Y%m%d%H%M%S")-$$(printf "%04d\n" $(BUILD_ID))-$(BUILD_HASH).tar.gz.download
+	mk_test $(@) -f $(TMP_DIR)/workspace-$(PROJECT_NAME_SHORT)-$$(date --date=$(BUILD_DATE) -u +"%Y%m%d%H%M%S")-$$(printf "%04d\n" $(BUILD_ID))-$(BUILD_HASH).tar.gz.download
