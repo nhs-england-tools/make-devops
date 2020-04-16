67760e1fe6cafe8db4df3f11a3b4024c65e25f5e
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2020-04-16 07:26:54 +0100

    Fix tests

--- build/automation/test/python.test.mk
+++ build/automation/test/python.test.mk
@@ -7,13 +7,13 @@ test-python: \
 test-python-virtualenv:
 	mk_test_skip_if_not_macos $(@) ||:
 	# act
-	make python-env-setup
+	make python-virtualenv
 	# assert
 	mk_test $(@) "$(PYTHON_VERSION)" = "$$(python --version | awk '{ print $$2 }')"
 
 test-python-virtualenv-clean:
 	mk_test_skip_if_not_macos $(@) ||:
 	# act
-	make python-env-clean
+	make python-virtualenv-clean
 	# assert
 	mk_test $(@) "system" = "$$(pyenv version | grep -o ^system)"
