2733b303de4fec3886721be798c0a422531e18b5
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2020-07-30 07:53:32 +0100

    Allow to run SQL scripts as different user

--- build/automation/lib/docker/image/postgres/assets/sbin/run.sh
+++ build/automation/lib/docker/image/postgres/assets/sbin/run.sh
@@ -14,13 +14,33 @@ if [ "sql" == "$1" ]; then
   echo "Running SQL: $sql"
   run_psql -c "$sql"
 elif [ "script" == "$1" ] || [ "scripts" == "$1" ]; then
-  dir=${2:-/data}
+  # Process arguments
+  ([ -n "$2" ] && [[ $2 != --run-as* ]]) && dir=$2 || dir=/data
+  for arg in "$@"; do
+      if [[ $arg = --run-as-connection-details-prefix=* ]]; then
+        connection_details_prefix=$(echo $arg | sed "s;--run-as-connection-details-prefix=;;g")
+      fi
+      if [[ $arg = --run-as-script-name-pattern=* ]]; then
+        script_name_pattern=$(echo $arg | sed "s;--run-as-script-name-pattern=;;g")
+      fi
+  done
   i=0
+  # Run scripts
   for file in $dir/*; do
-    echo "Running script: '$file'"
-    [[ $file == *.sql ]] && run_psql -f $file
-    [[ $file == *.sql.gz ]] && gunzip -c $file | run_psql
-    echo
+    (
+      # Check for alternative connection details
+      for item in $(echo $script_name_pattern | sed "s;,; ;g"); do
+          if [[ $file = *$item* ]]; then
+            DB_NAME=$(eval echo "\$${connection_details_prefix}DB_NAME")
+            DB_USERNAME=$(eval echo "\$${connection_details_prefix}DB_USERNAME")
+            DB_PASSWORD=$(eval echo "\$${connection_details_prefix}DB_PASSWORD")
+          fi
+      done
+      echo "Running script: '$file'"
+      [[ $file == *.sql ]] && run_psql -f $file
+      [[ $file == *.sql.gz ]] && gunzip -c $file | run_psql
+      echo
+    )
     i=$((i+1))
   done
   if [ $i -eq 0 ]; then
