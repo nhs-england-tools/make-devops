c74f935056c23cf756b3e4012faa0431329c0b2e
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2020-08-03 11:58:48 +0100

    Exclude .drawio from formatting

--- build/automation/etc/githooks/scripts/editorconfig-pre-commit.sh
+++ build/automation/etc/githooks/scripts/editorconfig-pre-commit.sh
@@ -5,9 +5,9 @@
 if [ "$PROJECT_NAME" = "$DEVOPS_PROJECT_NAME" ]; then
   docker run --rm \
     --volume=$PWD:/check mstruebing/editorconfig-checker \
-    ec --exclude 'markdown|linux-amd64$|\.p12$'
+    ec --exclude 'markdown|linux-amd64$|\.drawio|\.p12$'
 else
   docker run --rm \
     --volume=$PWD:/check mstruebing/editorconfig-checker \
-    ec --exclude 'build/automation|markdown|linux-amd64$|\.p12$'
+    ec --exclude 'build/automation|markdown|linux-amd64$|\.drawio|\.p12$'
 fi
