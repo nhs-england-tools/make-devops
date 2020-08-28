499bf8e1b51ad47a33f801bd4fb5d9135588f07b
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2020-08-28 10:46:08 +0100

    Add Slack module

--- build/automation/lib/slack.mk
+++ build/automation/lib/slack.mk
@@ -1,1 +1,14 @@
-# TODO: Slack
+slack-send-standard-notification: ### Send standard notification - mandatory: NAME=[notification template name],SLACK_WEBHOOK_URL
+	make slack-send-notification FILE=$(LIB_DIR)/slack/$(NAME).json
+
+slack-send-notification: ### Send notification based on a template - mandatory: FILE=[template file],SLACK_WEBHOOK_URL
+	message=$$(make slack-render-template FILE=$(FILE))
+	curl --request POST --header "Content-type: application/json" --data "$$message" $(SLACK_WEBHOOK_URL)
+
+slack-render-template: ### Render message content from a template - mandatory: FILE=[template file]
+	make -s file-copy-and-replace SRC=$(FILE) DEST=$(TMP_DIR)/$(@).json > /dev/null 2>&1 && trap "{ rm -f $(TMP_DIR)/$(@).json; }" EXIT
+	file=$$(cat $(TMP_DIR)/$(@).json)
+	echo $$file
+
+.SILENT: \
+	slack-render-template
