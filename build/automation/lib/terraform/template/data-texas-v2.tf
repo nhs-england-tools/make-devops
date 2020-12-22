f7caeacb7c612ba36eedf9c95ba10dac5936de25
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2020-12-22 18:08:51 +0000

    Single state store bucket

--- build/automation/lib/terraform/template/data-texas-v2.tf
+++ build/automation/lib/terraform/template/data-texas-v2.tf
@@ -4,24 +4,24 @@
 data "terraform_remote_state" "networking" {
   backend = "s3"
   config = {
-    key    = "${var.terraform_state_key_shared_infra}/networking/terraform.state"
-    bucket = var.terraform_state_store_shared_infra
+    key    = "${var.terraform_state_key_shared}/networking/terraform.state"
+    bucket = var.terraform_state_store
     region = var.aws_region
   }
 }
 data "terraform_remote_state" "service-roles" {
   backend = "s3"
   config = {
-    key    = "${var.terraform_state_key_shared_infra}/service-roles/terraform.state"
-    bucket = var.terraform_state_store_shared_infra
+    key    = "${var.terraform_state_key_shared}/service-roles/terraform.state"
+    bucket = var.terraform_state_store
     region = var.aws_region
   }
 }
 data "terraform_remote_state" "terraform-state" {
   backend = "s3"
   config = {
-    key    = "${var.terraform_state_key_shared_infra}/terraform-state/terraform.state"
-    bucket = var.terraform_state_store_shared_infra
+    key    = "${var.terraform_state_key_shared}/terraform-state/terraform.state"
+    bucket = var.terraform_state_store
     region = var.aws_region
   }
 }
@@ -29,5 +29,5 @@ data "terraform_remote_state" "terraform-state" {
 # ==============================================================================
 # Terraform state keys and store set by the Make DevOps automation scripts
 
-variable "terraform_state_store_shared_infra" {}
-variable "terraform_state_key_shared_infra" {}
+variable "terraform_state_store" {}
+variable "terraform_state_key_shared" {}
