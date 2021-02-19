ea5349d736d16b28f4188f7fc6249b0f8291dc54
Matthew Begley <60427904+mabe13@users.noreply.github.com>
2021-02-19 08:28:43 +0000

    Project make target refinements  (#17)

--- build/automation/lib/project.mk
+++ build/automation/lib/project.mk
@@ -94,11 +94,11 @@ project-branch-deploy: ### Check if development branch can be deployed automatic
 	[ $$(make project-branch-test) == true ] && echo true && exit 0
 	echo false
 
-project-branch-test: ### Check if development branch can be tested automatically - return: true|false
+project-branch-test: ### Check if development branch can be tested automatically - optional KEYWORDS=[keywords,comma,separated] - return: true|false
+	keywords=$(or $(KEYWORDS) || test,func-test,perf-test,sec-test)
 	[[ $(BUILD_BRANCH) =~ $(GIT_BRANCH_PATTERN_MAIN) ]] && echo true && exit 0
 	[[ $(BUILD_BRANCH) =~ $(GIT_BRANCH_PATTERN_PREFIX)/$(GIT_BRANCH_PATTERN_SUFFIX) ]] && \
-		[ $$(make project-message-contains KEYWORD=test,func-test,perf-test,sec-test) == true ] && \
-			echo true && exit 0
+	[ $$(make project-message-contains KEYWORD=$$keywords) == true ] && echo true && exit 0
 	echo false
 
 project-branch-func-test: ### Check if development branch can be tested (functional) automatically - return: true|false
@@ -125,7 +125,7 @@ project-branch-sec-test: ### Check if development branch can be tested (security
 project-message-contains: ### Check if git commit message contains any give keyword - mandatory KEYWORD=[comma-separated keywords]
 	msg=$$(make git-msg)
 	for str in $$(echo $(KEYWORD) | sed "s/,/ /g"); do
-		echo "$$msg" | grep -E '[ci .*]' | grep -Eoq "\[ci .*$${str}[^-].*" && echo true && exit 0
+		echo "$$msg" | grep -E '[ci .*]' | grep -Eoq "\[ci.*[^-]$${str}[^-].*" && echo true && exit 0
 	done
 	echo false
 
