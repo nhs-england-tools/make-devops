b54efe06caf8577acd8d32005e2e689b2b0d4926
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2021-05-03 21:21:35 +0100

    Refactor techradar version detection

--- build/automation/lib/techradar/tech-python.sh
+++ build/automation/lib/techradar/tech-python.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 if ! which python > /dev/null 2>&1; then
-    exit 0
+  exit 0
 fi
 
-version="$(python --version | awk '{ print $2 }')"
+version="$(python --version 2>&1 | awk '{ print $2 }')"
 dependencies=$(
-    pip list | while read -r line; do
-        echo $line | grep -qi ^package && continue
-        echo $line | grep -q ^- && continue
-        package=$(echo $line | awk '{ print $1 }')
-        version=$(echo $line | awk '{ print $2 }')
-        printf "\"${package}\":\"${version}\","
-    done
+  pip list 2> /dev/null | while read -r line; do
+    echo $line | grep -qi ^package && continue
+    echo $line | grep -q ^- && continue
+    package=$(echo $line | awk '{ print $1 }')
+    version=$(echo $line | awk '{ print $2 }')
+    printf "{\"name\":\"${package}\",\"version\":\"${version}\"},"
+  done
 )
 
-printf "\"python\":{\"version\":\"${version}\",\"dependencies\":{$(printf "$dependencies" | head -c -1)}}"
+printf "\"python\":{\"version\":\"${version}\",\"dependencies\":[$(printf "$dependencies" | head -c -1)]}"
