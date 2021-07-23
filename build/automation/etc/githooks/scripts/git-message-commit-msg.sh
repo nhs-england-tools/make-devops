d60cedb9ddeb219011893bc5904a8beab45ef641
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2021-07-23 23:09:34 +0100

    Improve workflows and githooks (#54)

--- build/automation/etc/githooks/scripts/git-message-commit-msg.sh
+++ build/automation/etc/githooks/scripts/git-message-commit-msg.sh
@@ -5,7 +5,7 @@ message="$(cat $1)"
 if [ $(make git-check-if-commit-msg-is-correct BUILD_COMMIT_MESSAGE="$message") == false ]; then
   tput setaf 202
   printf "\n\n  $(echo $0 | sed "s;$PWD/;;"): Commit message '$message' does not meet the accepted convention\n\n"
-  make show-configuration | grep GIT_COMMIT_MESSAGE_
+  make show-configuration | grep GIT_COMMIT_MESSAGE_ && printf "\nPlease, for more details see the 'build/automation/test/git.test.mk' file.\n"
   tput sgr0
   exit 1
 fi
