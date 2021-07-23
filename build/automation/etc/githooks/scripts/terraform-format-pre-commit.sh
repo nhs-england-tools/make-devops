632920b3843b62cf05f2eef7a4b0a054f8c56999
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2021-07-23 22:33:09 +0100

    Improve GitHub workflows

--- build/automation/etc/githooks/scripts/terraform-format-pre-commit.sh
+++ build/automation/etc/githooks/scripts/terraform-format-pre-commit.sh
@@ -2,7 +2,7 @@
 
 [ $(make project-check-if-tech-is-included-in-stack NAME=terraform) == false ] && exit 0
 
-if [ "$PROJECT_NAME" = "$DEVOPS_PROJECT_NAME" ]; then
+if [ "$PROJECT_NAME" == "$DEVOPS_PROJECT_NAME" ]; then
   if [ $(make git-check-if-commit-changed-directory DIR=build/automation/lib/terraform/template PRECOMMIT=true) == true ]; then
     if ! make -s terraform-fmt DIR=build/automation/lib/terraform/template OPTS="-check -list=false" 2> /dev/null; then
       tput setaf 202
