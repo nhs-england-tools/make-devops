5fa49f747c82a992d8c75de4acf6c8aac5ce1862
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2021-09-22 07:32:19 +0100

    Refactor project setup shell script

--- install-project.sh
+++ install-project.sh
@@ -2,23 +2,22 @@
 set -e
 
 function main() {
-  download
   dir=$(pwd)
-  cd "$HOME/.make-devops"
+  download
   make devops-copy DIR=$dir
   finish
 }
 
 function download() {
-  curl -L \
-    "https://github.com/nhsd-exeter/make-devops/tarball/${BRANCH_NAME:-master}?$(date +%s)" \
-    -o /tmp/make-devops.tar.gz
-  tar -zxf /tmp/make-devops.tar.gz -C /tmp
-  rm -rf \
-    /tmp/make-devops.tar.gz \
-    /tmp/make-devops* \
-    "$HOME/.make-devops"
-  mv /tmp/nhsd-exeter-make-devops-* "$HOME/.make-devops"
+  if ! [ -d "$HOME/.make-devops/.git" ]; then
+    cd "$HOME"
+    rm -rf make-devops .make-devops
+    git clone https://github.com/nhsd-exeter/make-devops.git
+    mv make-devops .make-devops
+  fi
+  cd "$HOME/.make-devops"
+  git pull --all
+  git checkout ${BRANCH_NAME:-master}
 }
 
 function finish() {
